8ce4f19ca10782aec49173c78a697e349fe25a1e
Miguelon <143040697+Miguelon1708@users.noreply.github.com>
2026-02-17 11:59:43 +0100

    Update managerConfig.sh

--- managerFiles/managerConfig.sh
+++ managerFiles/managerConfig.sh
@@ -3,3 +3,5 @@
 awk '/"data": \{/{found=1; print; next} 
      found && /"properties": \{/ {print; system("cat variableTypes.txt"); found=0; next}
      1' /etc/filebeat/wazuh-template.json > temp && mv temp /etc/filebeat/wazuh-template.json
+
+sudo filebeat setup -index-management
