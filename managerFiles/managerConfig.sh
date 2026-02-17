17dfb88674a62d5e3f797bb74ff3e3975c8fd3f3
Miguelon <143040697+Miguelon1708@users.noreply.github.com>
2026-02-17 10:40:29 +0100

    updatedManager

---
+++ managerFiles/managerConfig.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+awk '/"data": \{/{found=1; print; next} 
+     found && /"properties": \{/ {print; system("cat variableTypes.txt"); found=0; next}
+     1' /etc/filebeat/wazuh-template.json > temp && mv temp /etc/filebeat/wazuh-template.json
